81762de08d9b650474b655570a7f2f9b01229677
Insula <66838181+Insula415@users.noreply.github.com>
2021-07-07 15:53:18 +0100

    Create content.js

---
+++ content.js
@@ -0,0 +1,36 @@
+mainImage = "https://i.imgur.com/yNEGGop.png";
+mainText = "You better get back to work";
+
+url = window.location.href;
+if(url.match("www.youtube")) {
+    console.log("Follow Insula415 on TikTok!")
+
+    const headers = document.getElementsByTagName("yt-formatted-string");
+    for (let i=0; i<headers.length; i++) {
+        headers[i].innerText = mainText;
+    }
+
+    const imgs = document.getElementsByTagName("img")
+    for (let i=0;i<imgs.length;i++) {
+        img[i].src = mainImage;
+    }
+}
+
+if(url.match("www.netflix")) {
+    const desc = document.getElementsByClassName("synopsis");
+    for (let i=0;i<desc.length;i++) {
+        desc[i].innerText = mainText;
+    }
+    const head = document.getElementsByClassName("supplemental-message");
+    for (let i=0;i<head.length;i++) {
+        head[i].innerText = mainText;
+    }
+    const h = document.getElementsByTagName("h2");
+    for (let i=0;i<h.length;i++) {
+        h[i].innerText = mainText;
+    }
+    const imgs = document.getElementsByTagName("img");
+    for (let i=0,l=imgs.length;i<l;i++){
+        imgs[i].src = mainImage;
+    }
+}
